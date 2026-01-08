cbe3c7145f8a251127f2ab0c1f6760d58ff18c0e
Nithish A <nithishmaster03@gmail.com>
2026-01-08 16:29:42 +0000

    ver11

---
+++ nithish.py
@@ -0,0 +1,40 @@
+import re
+
+# Input: comma separated passwords
+passwords = input().split(",")
+
+valid_passwords = []
+
+for pwd in passwords:
+    pwd = pwd.strip()   # remove extra spaces
+
+    if (6 <= len(pwd) <= 12 and
+        re.search("[a-z]", pwd) and
+        re.search("[A-Z]", pwd) and
+        re.search("[0-9]", pwd) and
+        re.search("[$#@]", pwd)):
+        
+        valid_passwords.append(pwd)
+
+# Output valid passwords separated by comma
+print(",".join(valid_passwords))
+import re
+
+# Input: comma separated passwords
+passwords = input().split(",")
+
+valid_passwords = []
+
+for pwd in passwords:
+    pwd = pwd.strip()   # remove extra spaces
+
+    if (6 <= len(pwd) <= 12 and
+        re.search("[a-z]", pwd) and
+        re.search("[A-Z]", pwd) and
+        re.search("[0-9]", pwd) and
+        re.search("[$#@]", pwd)):
+        
+        valid_passwords.append(pwd)
+
+# Output valid passwords separated by comma
+print(",".join(valid_passwords))
