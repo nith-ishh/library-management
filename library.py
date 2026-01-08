fb1a09847e0b1858da1bc9eb3a31ba327a43141d
Nithish A <nithishmaster03@gmail.com>
2026-01-08 13:55:20 +0000

    ver7

--- library.py
+++ library.py
@@ -1,62 +1,12 @@
-# Simple Library Management System
+# Simple Login System
 
-library = []
+USERNAME = "admin"
+PASSWORD = "1234"
 
-def add_book(book_name):
-    library.append(book_name)
-    print(f"Book '{book_name}' added successfully.")
+username = input("Enter username: ")
+password = input("Enter password: ")
 
-def view_books():
-    if not library:
-        print("Library is empty.")
-    else:
-        print("Books available in library:")
-        for book in library:
-            print("-", book)
-
-def issue_book(book_name):
-    if book_name in library:
-        library.remove(book_name)
-        print(f"Book '{book_name}' issued successfully.")
-    else:
-        print("Book not available.")
-
-def return_book(book_name):
-    library.append(book_name)
-    print(f"Book '{book_name}' returned successfully.")
-
-# Menu-driven program
-while True:
-    print("\n--- Library Management System ---")
-    print("1. Add Book")
-    print("2. View Books")
-    print("3. Issue Book")
-    print("4. Return Book")
-    print("5. Exit")
-
-    choice = input("Enter your choice: ")
-
-    if choice == "1":
-        name = input("Enter book name: ")
-        add_book(name)
-
-    elif choice == "2":
-        view_books()
-
-    elif choice == "3":
-        name = input("Enter book name to issue: ")
-        issue_book(name)
-
-    elif choice == "4":
-        name = input("Enter book name to return: ")
-        return_book(name)
-
-    elif choice == "5":
-        print("Exiting Library System.")
-        break
-
-    else:
-        print("Invalid choice. Try again.")
-
-print("Thank you for using the Library Management System!")
-print("Program terminated successfully.")
+if username == USERNAME and password == PASSWORD:
+    print("Login successful!")
+else:
+    print("Invalid username or password.")
